34c753052aa5647304e4707600d8decb56930bfd
euguroglu <e.uguroglu@yahoo.com>
2020-12-19 00:36:11 +0300

    py file finished

--- adoption_site.py
+++ adoption_site.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask,render_template,url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from forms import AddForm, DelForm
 
 app = Flask(__name__)
 
@@ -27,3 +27,49 @@ class Puppy(db.Model):
 
     def __repr__(self):
         return f"Puppy name: {self.name}"
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/add',methods=["GET","POST"])
+def add_pup():
+
+    form = AddForm()
+
+    if form.validate_on_submit():
+
+        name = form.name.data
+        new_pup = Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('add.html',form=form)
+
+@app.route('/list')
+def list_pup():
+
+    puppies = Puppy.query.all()
+
+    return render_template("list.html",puppies=puppies)
+
+
+@app.route('/delete',methods=["GET","POST"])
+def del_pup():
+
+    form = DelForm()
+
+    if form.validate_on_submit():
+
+        id = form.id.Data
+        pup = Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+    return render_template('delete.html',form=form)
+
+if __name__ == "main":
+    app.run(debug=True)
